72fe6709d483ff80c772acccda320af89fa611b4
Sundar  Anbu <sundaranbu@Sundars-MacBook-Pro.local>
2026-08-04 00:15:59 +0530

    data

--- src/components/ChatbotWidget.jsx
+++ src/components/ChatbotWidget.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-const RAGBOT_BASE_URL = "http://8.231.118.245";
+const RAGBOT_BASE_URL = "https://8.231.118.245.nip.io";
 const RAGBOT_API_KEY = "a2ad388bccf7b613cb091230545f6aec40975eef9956ba1c2b00f8c8bb4d2c40";
 
 function ChatbotWidget() {
